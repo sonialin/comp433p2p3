040304ca464ef8a945ed307196e81bb62f83542d
tliu6@luc.edu <tliu6@luc.edu>
2016-10-30 23:59:47 -0500

    updated ReviewManageFacade

--- comp433p2p3/src/model/review/ReviewManagerFacade.java
+++ comp433p2p3/src/model/review/ReviewManagerFacade.java
@@ -1,23 +1,25 @@
 package model.review;
 
+import java.util.Set;
+
 import dal.ReviewDAO;
 
 public class ReviewManagerFacade{
 
 	/**
-	 * This class manages reviews for puoducts from cutomers
+	 * This class manages reviews for products from customers
 	 */
 	ReviewDAO rdao = new ReviewDAO();
 	
-	public void writeReview(String customerusername, String productID, String reviewcontent,int rate){
+	public void writeReview(String customerusername, int productID, String reviewcontent,int rate){
 			
 		rdao.writeReview(customerusername, productID, reviewcontent, rate);			
 		
 	}
 	
-	public void displayReview(String Poductname){
+	public Set<Review> displayReview(String Poductname){
 		
-		rdao.displayReview(Poductname);
+		return rdao.getReview(Poductname);
 		
 	}
 	
