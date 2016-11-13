2e9c67a144a12863be7df02dea6363e32080e4e2
Sonia Lin <sonia_lin@hotmail.com>
2016-11-13 12:12:53 -0600

    Changed order get media type to enable jason output

--- comp433p2p3/src/model/constant/Constant.java
+++ comp433p2p3/src/model/constant/Constant.java
@@ -1,9 +1,9 @@
 package model.constant;
 
 public class Constant {
-	public static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
-	public static final String USERNAME = "DBuser";
-	public static final String PASSWORD = "DBkaimen";
+	public static final String URL = "jdbc:mysql://localhost:3306/ecommerce?useSSL=false";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "123456";
 	public static final float TAXRATE = 9.97f;
 		
 }
--- comp433p2p3/src/service/resource/order/OrderResource.java
+++ comp433p2p3/src/service/resource/order/OrderResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.CacheControl;
@@ -32,7 +33,7 @@ public class OrderResource implements OrderService {
 	}
 	
 	@GET
-	@Produces({"application/xml" , "application/json"})
+	@Produces({MediaType.APPLICATION_JSON})
 	@Path("/getorder/{orderID}")
 	public OrderRepresentation getOrder(@PathParam("orderID") int orderID) {
 		System.out.println("GET METHOD Request from Client with orderRequest int ............." + orderID);
